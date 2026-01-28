e10599eec7ae464c084a2b5e967ad642c34e72c5
Allan Quiej <allan.quiej@gmail.com>
2026-01-28 13:44:26 -0600

    se quito pagina de indemnizacion

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage";
 import ServiciosPage from "./pages/ServiciosPage";
 import PrincipiosPage from "./pages/PrincipiosPage";
 import ContactoPage from "./pages/ContactoPage";
-import IndemnizacionPage from "./pages/IndemnizacionPage";
+//import IndemnizacionPage from "./pages/IndemnizacionPage";
 import Bono14Page from "./pages/Bono14Page";
 import AguinaldoPage from "./pages/AguinaldoPage";
 import ISRLaboralPage from "./pages/ISRLaboralPage";
@@ -24,7 +24,7 @@ function App() {
           <Route path="/servicios" element={<ServiciosPage />} />
           <Route path="/principios" element={<PrincipiosPage />} />
           <Route path="/contacto" element={<ContactoPage />} />
-          <Route path="/calculadora-indemnizacion" element={<IndemnizacionPage />} />
+          {/*<Route path="/calculadora-indemnizacion" element={<IndemnizacionPage />} />*/}
           <Route path="/calculadora-bono14" element={<Bono14Page />} />
           <Route path="/calculadora-aguinaldo" element={<AguinaldoPage />} />
           <Route path="/calculadora-isr-laboral" element={<ISRLaboralPage />} />
