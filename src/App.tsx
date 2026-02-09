561850e3ff2b06a7f35405452c0b0fc597162496
Allan Quiej <allan.quiej@gmail.com>
2026-02-08 18:06:26 -0600

    probando cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
 import ServiciosPage from "./pages/ServiciosPage";
 import PrincipiosPage from "./pages/PrincipiosPage";
@@ -20,7 +19,6 @@ import IVAPage from "./pages/IVAPage";
 function App() {
   return (
     <Router>
-      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
